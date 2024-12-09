0efe813c49d733086132b7576aebbe1ddc57f31f
JasperK04 <j.kleine.4@student.rug.nl>
2024-12-09 23:45:36 +0100

    add GET routes for users

--- app/api/users.py
+++ app/api/users.py
@@ -1,9 +1,18 @@
 from . import api
+from app import db
+import sqlalchemy as sa
+from flask import request
+from app.models.User import User
 
 
-@api.route("/users", methods=["GET"])
+@api.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    return db.get_or_404(User, id).to_dict()
+
+
+@api.route('/users', methods=['GET'])
 def get_users():
-    pass
-
-# TODO: Add more user stuff, e.g. user by id,
-#  edit user profile if logged in as that user, etc
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return User.to_collection_dict(sa.select(User), page, per_page,
+                                   'api.get_users')
--- app/models/User.py
+++ app/models/User.py
@@ -2,8 +2,35 @@ from sqlalchemy import Integer, String, Date, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+from flask import url_for
 
-class User(db.Model):
+
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = db.paginate(query, page=page, per_page=per_page,
+                                error_out=False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+
+class User(PaginatedAPIMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     password_hash: Mapped[str] = mapped_column(nullable=False)
@@ -20,3 +47,9 @@ class User(db.Model):
     def check_password(self, password:str):
         return check_password_hash(self.password_hash, password)
 
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'username': self.name
+        }
+        return data
