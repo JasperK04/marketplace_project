20e41c39461598fb04c68689f572558f5d29c239
JasperK04 <j.kleine.4@student.rug.nl>
2024-12-10 23:09:14 +0100

    add POST route and GET routes

--- app/api/listings.py
+++ app/api/listings.py
@@ -1,9 +1,56 @@
 from . import api
+from app import db
+import sqlalchemy as sa
+from flask import request, url_for
+from app.api.errors import bad_request
+from app.api.auth import token_auth
+
+from app.models.User import User
+from app.models.Listing import Listing
+from app.models.Category import Category
 
 
-@api.route("/listings", methods=["GET"])
+@api.route('/listings/<int:id>', methods=['GET'])
+def get_listing(id):
+    return db.get_or_404(Listing, id).to_dict()
+
+@api.route('/listings/', methods=['GET'])
+@api.route('/listings', methods=['GET'])
 def get_listings():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return Listing.to_collection_dict(sa.select(Listing), page, per_page,
+                                   'api.get_listings')
+
+
+@api.route('/listings/', methods=['POST'])
+@api.route('/listings', methods=['POST'])
+@token_auth.login_required
+def create_listing():
+    fields = ["title", "price", "description"]
+    data = request.get_json()
+    current_user: User = token_auth.current_user() #type: ignore
+    data['userID'] = current_user.id
+    if not (all(field in data for field in fields) and ('category' in data or 'categoryID' in data)):
+        return bad_request(f'must include: {", ".join(fields)} and category')
+    if not db.session.scalar(sa.select(User).where(
+            User.id == data['userID'])):
+        return bad_request('This user does not exist')
+    if 'category' in data:
+        if categoryID := db.session.scalar(sa.select(Category.id).where(
+                Category.name == data['category'])):
+            data['categoryID'] = categoryID
+        else:
+            return bad_request('This category does not exist')
+    elif 'categoryID' in data and not db.session.scalar(sa.select(Category).where(
+            Category.id == data['categoryID'])):
+        return bad_request('This category does not exist')
+    listing = Listing()
+    listing.from_dict(data, new_user=True)
+    db.session.add(listing)
+    db.session.commit()
+    return listing.to_dict(), 201, {'Location': url_for('api.get_listing',
+                                                     id=listing.id)}
 
 # TODO: Add more Listing stuff, e.g. listing by id, by price above/below something,
 #  edit listings if logged in as the user who created them,
