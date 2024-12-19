771b3714f027b813c7fe329fe8eff28130136964
JasperK04 <j.kleine.4@student.rug.nl>
2024-12-19 12:03:25 +0100

    replace Category API-Mixin with overarching API-Mixin

--- app/models/Category.py
+++ app/models/Category.py
@@ -1,32 +1,8 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
-from flask import url_for
 from app import db
 
-
-class PaginatedAPIMixin(object):
-    @staticmethod
-    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
-        resources = db.paginate(query, page=page, per_page=per_page,
-                                error_out=False)
-        data = {
-            'items': [item.to_dict() for item in resources.items],
-            '_meta': {
-                'page': page,
-                'per_page': per_page,
-                'total_pages': resources.pages,
-                'total_items': resources.total
-            },
-            '_links': {
-                'self': url_for(endpoint, page=page, per_page=per_page,
-                                **kwargs),
-                'next': url_for(endpoint, page=page + 1, per_page=per_page,
-                                **kwargs) if resources.has_next else None,
-                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
-                                **kwargs) if resources.has_prev else None
-            }
-        }
-        return data
+from mixin import PaginatedAPIMixin
 
 
 class Category(PaginatedAPIMixin, db.Model):
