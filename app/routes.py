09dd80647bc05688f30dfee5a930e3c67fac08fd
bruggenj <bruggenjoris@gmail.com>
2024-12-27 18:21:49 +0100

    Listing card component

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template
 
-from . import app
+from . import app, db
+from .models.Listing import Listing
 
 
 @app.route("/", methods=["GET"])
 @app.route("/index", methods=["GET"])
 def index():
-    return render_template("index.html")
+    listings = db.session.query(Listing).filter_by(sold=False).all()
+    return render_template("index.html", listings=listings)
