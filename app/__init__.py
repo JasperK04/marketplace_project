fda13b67b9fc8d3c6694ac5e1c2761e28a778895
JDRijke <janiekderijke@gmail.com>
2025-01-12 18:00:37 +0100

    added session to app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_session import Session
 from os import path, getenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -15,11 +16,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv(
     "DB_URL"
 ) or "sqlite:///" + path.join(basedir, "app.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = getenv("TRACK_MODIFICATIONS") or False
-
+app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config["SECRET_KEY"] = getenv("SECRET_KEY") or "secret-key"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+session = Session(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
