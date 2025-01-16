cd703aa9570f7a95ef0145fdf1f8107387a133eb
JDRijke <janiekderijke@gmail.com>
2025-01-16 22:08:31 +0100

    Added a function (don't know where to place it yet) to resize listing images and place them into the UPLOAD FOLDER and RESIZED FOLDER. The filepath is written to the database, along with some other info. 'original' is not the original image, but rather the 300x200 image. I don't know if this way of storing images is the best, but I think it's way easier than storing the bytesdata in the database itself. Also changed the listings and listing routes to incorporate the images. For the individual listing pages it works, but for the listings in the profile.html and index.html it does not. It's also weird that listings that are added cannot be seen in the index.html, but can be seen in the profile.html.

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,11 @@ from app.forms import LoginForm,RegistrationForm, ListingForm
 from app.models.User import User
 from app.models.Category import Category
 import sqlalchemy as sa
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from urllib.parse import urlsplit
 from werkzeug.utils import secure_filename
+import os
+from PIL import Image as IM
 
 
 from . import app, db
@@ -63,7 +65,7 @@ def register():
 
 @app.route('/profile/<user_id>')
 def profile(user_id):
-    user = db.get_or_404(sa.select(User).where(User.id == user_id))
+    user = db.get_or_404(User,user_id)
     listings = db.session.execute(
         sa.select(Listing).where(Listing.userID == user.id)).scalars()
     profile_pic = db.session.execute(
@@ -74,14 +76,24 @@ def profile(user_id):
 # Listings
 @app.route("/listings", methods=["GET"])
 def listings():
-    listings = db.session.query(Listing).all()
+    all_listings = db.session.query(Listing).all()
+    listings = []
+    for listing in all_listings:
+        image = db.session.execute(sa.select(Image).where(and_(Image.listingID==listing.id, Image.variant == 'original'))).scalar()
+        listings.append({
+            "id": listing.id,
+            "title": listing.title,
+            "price": listing.price,
+            "filename": image.filename if image else None
+        })
     return render_template("listings.html", listings=listings)
 
 
 @app.route("/listings/<int:listing_id>", methods=["GET"])
 def listing(listing_id):
     listing = db.get_or_404(Listing, listing_id).to_dict()
-    return render_template("listing.html", listing=listing)
+    image = db.session.execute(sa.select(Image).where(and_(Image.listingID==listing_id, Image.variant == 'resized'))).scalar()
+    return render_template("listing.html", listing=listing, image=image)
 
  
 
@@ -97,9 +109,42 @@ def add_listing():
                               price=form.price.data,userID=current_user.id)
         db.session.add(new_listing)
         db.session.flush()
-        new_image = Image(img=file.read(),filename=secure_filename(file.filename),mimetype=file.mimetype,
-                          type='listing',listingID=new_listing.id)
-        db.session.add(new_image)
+        new_images = resize_upload_image(file,new_listing)
+        db.session.add(new_images[0])
+        db.session.add(new_images[1])
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('add_listing.html',title='Add listing',form=form)
+
+
+def resize_upload_image(file, listing):
+    image = IM.open(file)
+    target_sizes = [(300, 200), (600, 400)]
+    folder_names = ['UPLOAD_FOLDER', 'RESIZED_FOLDER']
+    file_prefixes = ['', 'resized_']
+    variants = ['original','resized']
+    images = []
+
+    for size, folder, prefix, variant in zip(target_sizes, folder_names, file_prefixes, variants):
+        img = image.copy()
+        # if image is smaller than preferred size, thumbnail (resize with same aspect ratio) cannot be used 
+        if img.width < size[0] or img.height < size[1]:
+            img = img.resize(size,resample=1) # 1=LANCZOS resampling leads to higher quality pictures
+        else:
+            img.thumbnail(size,resample=1) 
+
+        filename = prefix + secure_filename(file.filename)
+        filepath = os.path.join(app.config[folder], filename)
+        os.makedirs(app.config[folder], exist_ok=True)
+        img.save(filepath)
+
+        new_image = Image(
+            filename=filename,
+            filepath=filepath,
+            type='listing',
+            listingID=listing.id,
+            variant=variant
+        )
+        images.append(new_image)
+
+    return images
